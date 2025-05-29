acbb55cc61cc0231687dc43eca217952d1cb34e4
Johnathon47 <hei.johnathon.2@gmail.com>
2025-05-29 17:04:53 +0300

    fix: fix the input enter the name because I use prompt for terminal not for html

--- carte.js
+++ carte.js
@@ -1,17 +1,17 @@
-const prompt = require("prompt-sync")();
 function menu() {
-    console.log("                               ----------------------------------");
-    console.log(" ");
-    console.log("                                 BIENVENU.E DANS LE JEU DE CARTE");
-    console.log("                                                 ");
-    console.log("                                                 ");
-    console.log(" ");
-    console.log("                               -----------------------------------");
 
-        let pseudo = prompt("Saisissez votre pseudo :  ");
-        console.log(`  \n    Bonjour ${pseudo}, voici la règle du jeu :\n    Tu as 3 carte au choix, l'eau bat le feu, le feu bat la plante, la plante bat l'eau.
-    En cas d'égalité, vous avez le choix de continuer ou d'arrêter.`);
-        console.log(`Maintenant que vous savez les règles du jeu, vous pouvez commencer la partie`);
+    document.getElementById("show-rules").innerText = "<h3> Bienvenue ! <h3/>"
+    // Utilise prompt natif du navigateur :
+    let pseudo = document.getElementById("pseudo").value;
+
+    document.getElementById("show-rules").innerText = `\n Bonjour ${pseudo}, voici la règle du jeu :\n
+    Tu as 3 cartes au choix :
+    - l'eau bat le feu
+    - le feu bat la plante
+    - la plante bat l'eau
+    En cas d'égalité, vous avez le choix de continuer ou d'arrêter.
+    
+    Maintenant que vous connaissez les règles du jeu, vous pouvez commencer la partie.`;
+
 }
-exports.main = menu;
 
